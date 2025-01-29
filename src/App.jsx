bc1533a6e8250220b4b4366ed9f7a89016c25364
susmitajadhav <45027764+susmitajadhav@users.noreply.github.com>
2025-01-29 19:26:00 +0530

    Fixed HashRouter issue and corrected routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
-// src/App.jsx
 import React from 'react';
-
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import HeroSection from './components/HeroSection';
 import Footer from './components/Footer';
 import AboutUs from './components/AboutUs';
 import ContactPage from './components/ContactPage';
 import ServicesPage from './components/ServicesPage';
-// import Services from './components/Services';
 import GetQuote from './components/GetQuote';
-// import Main from './components/Main';
 import Product from './components/Product';
 import BlogPage from './components/BlogPage';
 import ProductCard from './components/ProductCard';
@@ -26,12 +22,9 @@ const Home = () => {
     <div>
       <Header />
       <HeroSection />
-      <CompanyList/>
-      {/* <ProductCard/> */}
-      
-      <CompanyDetails/>
+      <CompanyList />
+      <CompanyDetails />
       <ServicesPage />
-    
       <BlogPage />
       <ContactPage />
       <Footer />
@@ -42,25 +35,23 @@ const Home = () => {
 // App Component
 const App = () => {
   return (
-    <Router>
+    <Router> {/* Changed to HashRouter for GitHub Pages */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/herosection" element={<HeroSection />} />
         <Route path="/footer" element={<Footer />} />
         <Route path="/productcard" element={<ProductCard />} />
-        <Route path="/blog/:id" component={<BlogDetailsPage />} />
+        <Route path="/blog/:id" element={<BlogDetailsPage />} /> {/* Fixed 'component' prop */}
         <Route path="/contactpage" element={<ContactPage />} />
         <Route path="/servicespage" element={<ServicesPage />} />
-        <Route path="/" element={<ServicesPage />} />
         <Route path="/service/:id" element={<ServiceDetailPage />} /> 
         <Route path="/getquote" element={<GetQuote />} />
-        {/* <Route path="/main" element={<Main />} /> */}
         <Route path="/aboutus" element={<AboutUs />} />
         <Route path="/product" element={<Product />} />
-        <Route path="/companydeatils" element={<CompanyDetails />} />
+        <Route path="/companydetails" element={<CompanyDetails />} /> {/* Fixed typo */}
         <Route path="/blogpage" element={<BlogPage />} />
         <Route path="/gallery" element={<Gallery />} />
-        <Route path="/compnaylist" element={<CompanyList />} />
+        <Route path="/companylist" element={<CompanyList />} /> {/* Fixed typo */}
       </Routes>
     </Router>
   );
